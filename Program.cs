75fc6d0e3235e872d52b033ee252cf5895371d65
Thiago <thiagonunesal1307@gmail.com>
2020-06-19 15:59:28 -0300

    Atividade corrigida

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AulaPOOIfoodAjuste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente thiago = new Cliente("Thiago");
+            thiago.Endereco = "Rua Aleatória";
+            Restaurante bk = new Restaurante("Burger King", "Rua 12");
+
+            Pedido pedido = new Pedido();
+            pedido.Cliente = thiago;
+            pedido.Restaurante = bk;
+
+            Console.WriteLine( pedido.EntregarPedido() );
+        }
+    }
+}
